f9dab82b1ceb99371cbfcd44f4704c5159a7aef1
Digina T D <diginatd1998@gmail.com>
2018-07-27 10:56:58 +0530

    attendence

---
+++ app/src/main/java/com/sample/eventattendance/Event/Event.kt
@@ -0,0 +1,8 @@
+package com.sample.eventattendance.Event
+
+class Event(val id:String,
+            val name: String,
+            val date: String,
+            val venue:String,
+            val days:Int,
+            val noOfParticipants: Int)
